98df4c99440fb0f2f4e8942db8f4d1ab2a9971ae
Shijie_Li <lsj72123@gmail.com>
2024-03-16 14:06:46 +1100

    checkpoint_utils fix save_interval_updates

--- third_party/fairseq/fairseq/checkpoint_utils.py
+++ third_party/fairseq/fairseq/checkpoint_utils.py
@@ -72,7 +72,9 @@ def save_checkpoint(cfg: CheckpointConfig, trainer, epoch_itr, val_loss):
         end_of_epoch and not cfg.no_epoch_checkpoints and epoch % cfg.save_interval == 0
     )
     checkpoint_conds["checkpoint_{}_{}{}.pt".format(epoch, updates, suffix)] = (
-        not end_of_epoch and cfg.save_interval_updates > 0 and updates % cfg.save_interval_updates == 0
+        # interval-based save can happen at end_of_epoch, so we remove this constraints
+        cfg.save_interval_updates > 0
+        and updates % cfg.save_interval_updates == 0
     )
     checkpoint_conds["checkpoint_best{}.pt".format(suffix)] = val_loss is not None and (
         not hasattr(save_checkpoint, "best") or is_better(val_loss, save_checkpoint.best)
